0d5d6a7341357b1e129794612dcb5a5a561a535b
robmonte <17119716+robmonte@users.noreply.github.com>
2025-07-11 20:10:53 -0500

    fix image name

--- vm.tf
+++ vm.tf
@@ -9,7 +9,7 @@ resource "google_compute_instance" "my_instance" {
   zone         = "asia-northeast1-a"
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
       size  = 100
     }
   }
